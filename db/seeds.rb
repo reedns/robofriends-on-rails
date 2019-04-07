b6d2acc437d154af803f99e442345065bf0d34a6
Reed Nelson-Saunders <reedns@hotmail.com>
2019-04-07 16:03:06 -0700

    add seeds for robofriend model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+12.times do
+  Robofriend.create(name: Faker::Name.name, email: Faker::Internet.email)
+end
