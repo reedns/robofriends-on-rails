f20c80714b027f9759f0d9e40fc97c54c33d80e5
Reed Nelson-Saunders <reedns@hotmail.com>
2019-04-15 16:10:20 -0700

    load robots with redux

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -4,50 +4,49 @@ import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { setSearchField } from '../actions'
+import { setSearchField, requestRobots } from '../actions'
 import './App.css';
 
 const mapStateToProps = (state) => {
-  return { searchField: state.searchField }
+  return {
+    searchField: state.searchRobots.searchField,
+    isPending: state.requestRobots.isPending,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return { onSearchChange: (event) => dispatch(setSearchField(event.target.value)) }
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robots: [],
-      quote: {}
-    }
-  }
-
   componentDidMount() {
     console.log(this.props)
-    fetch('/api/robofriends')
-      .then(response => response.json())
-      .then(robofriends => this.setState({ robots: robofriends }));
+    this.props.onRequestRobots();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return(
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
+    return isPending ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      );
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
-import tunkMiddleware from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers'
+import { searchRobots, requestRobots } from './reducers'
 
 const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(
-  searchRobots,
+  rootReducer,
   applyMiddleware(thunkMiddleware, logger)
 );
 
